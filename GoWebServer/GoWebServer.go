afc3d2c856c44208ad1ec4aaeafabf25c95b3938
Andrew-M-C <laplacezhang@126.com>
2019-01-13 21:07:51 +0800

    Add url analyzing logic

--- GoWebServer/GoWebServer.go
+++ GoWebServer/GoWebServer.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"net/http"
+	log "amc/GoLogger"
 	"github.com/TarsCloud/TarsGo/tars"
 )
 
+type HttpHandler func(http.ResponseWriter, *http.Request)
+var httpHandlers = map[string]HttpHandler {
+	"/hello-tars":	HttpHelloHandler,
+	"/tars":		HttpTarsHandler,
+}
+
+/**
+ * reference: [golang获取完整的url](https://www.tuicool.com/articles/juuu2qm)
+ */
+func httpRootHandler(w http.ResponseWriter, r *http.Request) {
+	host := r.Host
+	url := r.RequestURI
+	log.Info("Request: " + host + url);
+	handler, found := httpHandlers[url]
+	if false == found {
+		w.Write([]byte("404 " + url + " not found"))
+	} else {
+		handler(w, r)
+	}
+}
+
 func main() {
-	mux := &tars.TarsHttpMux{}
-	mux.HandleFunc("/", HttpRootHandler)
 	cfg := tars.GetServerConfig()
-	tars.AddHttpServant(mux, cfg.App+"."+cfg.Server+".GoWebObj") //Register http server
+
+	helloMux := &tars.TarsHttpMux{}
+	helloMux.HandleFunc("/", httpRootHandler)
+	tars.AddHttpServant(helloMux, cfg.App+"."+cfg.Server+".GoWebObj")
+
 	tars.Run()
 }
